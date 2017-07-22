7234104506ae227a1cb151c8f9b45b02d52e85dd
pa7lux <pa7lux@gmail.com>
2017-07-22 16:25:48 +0300

    added day5

---
+++ day5/myfirstjsscript/myjs.js
@@ -0,0 +1,5 @@
+let name = prompt('Кто сам?');
+let where = prompt('С какого раёна?');
+let fan = prompt('За кого болеешь?');
+
+console.log(name+', вроде норм ты пацан, я сам из '+ where+' и болею за '+ fan);
